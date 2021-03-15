d1cc48a5ecdf0e8e74c096d20bdecbf06e016696
NATA-GRECU <natagrecu34@gmail.com>
2021-03-15 17:27:46 -0300

    refactor(project): Alterando cores do site

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,10 +7,11 @@ let assuntoOk = false
 
 function verificaEmail() {
     let txtEmail = document.querySelector("#txtEmail")
-    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    if (email.value.indexOf("@") == -1 && email.value.indexOf(".") == -1) {
         txtEmail.innerHTML = "E-mail inválido"
         txtEmail.style.color = "red"
-    } else {
+    } 
+    else {
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = "green"
         emailOk = true
